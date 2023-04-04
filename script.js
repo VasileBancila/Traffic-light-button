419371e552cd86c22587a9e22f36fd1aa1c3951c
Vasile Bancila <bancilavasile93@gmail.com>
2023-04-04 21:39:59 +0200

    modified the script and added a file style

--- script.js
+++ script.js
@@ -10,5 +10,8 @@ const colors = ['yellow', 'green', 'red'];
 
 function onClick() {
     btn.style.backgroundColor = colors[index];
-    index = index >= colors.length - 1 ? 0 : index + 1;
+    if (index >= colors.length - 1) {
+        index = 0;
+    }
+    index++;
 }
